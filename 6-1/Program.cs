58a3f0665076068126f3ee11ab02ae488fbe688d
naduhawww <117995427+naduhawww@users.noreply.github.com>
2022-11-30 19:12:53 +0700

    Add files via upload

---
+++ 6-1/Program.cs
@@ -0,0 +1,45 @@
+﻿
+// Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+
+double[] mapToInt(string[] strings)
+{
+    double[] numbers = new double[strings.Length];
+
+    for (int i = 0; i < strings.Length; i++)
+    {
+        numbers[i] = Convert.ToDouble(strings[i]);
+    }
+
+    return numbers;
+}
+
+int countPositive(double[] numbers)
+{
+    int count = 0;
+    foreach (var number in numbers)
+    {
+        if (number > 0)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+Console.WriteLine("Введите некоторое количаство(М) чисел, разделяя их пробелами");
+
+string[] strNumbers = Console.ReadLine().Split(" ");
+
+try
+{
+    double[] numbers = mapToInt(strNumbers);
+    Console.WriteLine("Количество чисел больше 0: " + countPositive(numbers));
+}
+catch (Exception e)
+{
+    Console.WriteLine("Некорректное значение");
+}
---
+++ 6-2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+//  значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double calculateY(double x, double k, double b)
+{
+    return (k * x) + b;
+}
+double findCrossingX(double k1, double b1, double k2, double b2)
+{
+    return (b2 - b2) / (k1 - k2);
+}
+
+Console.WriteLine("Введите коэффициент наклона первой линии, k1:");
+double k1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите слагаемое первой линии, b1:");
+double b1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите коэффициент наклона второй линии, k2:");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите слагаемое второй линии, b2:");
+double b2 = Convert.ToDouble(Console.ReadLine());
+
+if (k1 == k2) // пренебрегаю здесь фактором точности чисел с плавающей точкой
+{
+    Console.WriteLine("Линии параллельны");
+}
+else
+{
+    double crossingX = findCrossingX(k1, b1, k2, b2);
+    double crossingY = calculateY(crossingX, k1, b1);
+
+    Console.WriteLine($"Точка пересечения имеет координаты {String.Format("{0:F2}", crossingX)} {String.Format("{0:F2}", crossingY)}");
+}
